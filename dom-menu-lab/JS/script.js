2d9e96cf883dbff8359f4e6ded716807a6977bcb
android718 <100258018+android718@users.noreply.github.com>
2022-03-14 19:38:38 -0700

    first commit

---
+++ dom-menu-lab/JS/script.js
@@ -0,0 +1,14 @@
+// Menu data structure
+var menuLinks = [
+    {text: 'about', href: '/about'},
+    {text: 'catalog', href: '/catalog'},
+    {text: 'orders', href: '/orders'},
+    {text: 'account', href: '/account'},
+  ];
+
+  var main = [mainEL
+  ]
+   var mainEL = (--main-bg)
+  
+
+  
